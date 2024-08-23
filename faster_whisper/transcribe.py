bc9c334b99412ee2700909f64906883b367db836
luisalmeidabranco <luisalmeidabranco@accenture.com>
2024-08-23 15:20:26 +0100

    Customized the if for auto

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -157,7 +157,12 @@ class BatchedInferencePipeline:
             return device
         elif isinstance(device, str):
             if device == "auto" and self.model.device == "auto":
-                device = "cuda" if torch.cuda.is_available() else "cpu"
+                if torch.cuda.is_available():
+                    device = "cuda"
+                elif torch.backends.mps.is_available():
+                    device = "mps"
+                else:
+                    device ="cpu"
             elif device == "auto":
                 device = self.model.device
             elif device == "mps":
